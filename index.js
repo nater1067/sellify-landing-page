c9979d1ffd3470e9dc247d931fcdc292a62973aa
Nate Turner <nater1992@gmail.com>
2018-05-30 06:56:07 -0700

    more example steps

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const moment = require('moment');
 const rp = require('request-promise-native');
 
 const sendEvent = ({event_name, productId, prospectId, additionalInfo}) => {
-  return {
+  const request = {
     method: 'POST',
     uri: 'http://localhost:8081/events',
     headers: {
@@ -20,53 +20,8 @@ const sendEvent = ({event_name, productId, prospectId, additionalInfo}) => {
       reportedTime: moment().format("YYYY-MM-DDTHH:mm:ss.SSS")
     }),
   };
-};
 
-app.use(function (req, res, next) {
-  console.log('Time:', Date.now());
-  next()
-});
-
-app.post('/buyGadget', (req, res) => {
-  console.info("buying gadget x!");
-
-  const options = sendEvent({
-    event_name: "prospect_clicked_buy",
-    productId: 123,
-    prospectId: "abc",
-    additionalInfo: {
-      "product_name": "Gadget X"
-    },
-  });
-
-  rp(options)
-    .then(function (parsedBody) {
-      console.log(parsedBody);
-      res.send("purchased!");
-      // POST succeeded...
-    })
-    .catch(function (err) {
-      console.error(err);
-      res.send("failed!");
-      // POST failed...
-    });
-
-});
-
-app.use('/', function (req, res, next) {
-  console.log('Time:', Date.now());
-  const options = sendEvent({
-    event_name: "prospect_reached_landing_page",
-    productId: 123,
-    prospectId: "abc",
-    additionalInfo: {
-      "landing_page_host": req.headers.host,
-      "landing_page_url": req.url,
-      "user-agent": req.headers["user-agent"],
-    },
-  });
-
-  rp(options)
+  rp(request)
     .then(function (parsedBody) {
       console.log(parsedBody);
       // res.send("purchased!");
@@ -77,6 +32,43 @@ app.use('/', function (req, res, next) {
       console.error("failed to send analytics to sellify")
       // POST failed...
     });
+};
+
+app.use(function (req, res, next) {
+  console.log('Time:', Date.now());
+  next()
+});
+
+app.post('/buyGadget', (req, res) => {
+  console.info("buying gadget x!");
+
+  setTimeout(() => {
+    sendEvent({
+      event_name: "customer_completed_payment",
+      productId: 123,
+      prospectId: "abc",
+      additionalInfo: {
+        "product_name": "Gadget X"
+      },
+    });
+    res.send("")
+  }, 750)
+});
+
+app.use('/', function (req, res, next) {
+  console.log('Time:', Date.now());
+  sendEvent({
+    event_name: "prospect_reached_landing_page",
+    productId: 123,
+    prospectId: "abc",
+    additionalInfo: {
+      "landing_page_host": req.headers.host,
+      "landing_page_url": req.url,
+      "user-agent": req.headers["user-agent"],
+    },
+  });
+
+
 
   return express.static(path.join(__dirname, 'public'))(req, res, next)
 });
