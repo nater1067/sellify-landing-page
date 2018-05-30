e7a7e64b727059a395f7af5342eec4ac92049d92
Nate Turner <nater1992@gmail.com>
2018-05-30 06:12:16 -0700

    more fake events!

--- index.js
+++ index.js
@@ -1,44 +1,84 @@
-const express = require('express')
-const app = express()
-const path = require('path')
+const express = require('express');
+const app = express();
+const path = require('path');
+const moment = require('moment');
 
-const rp = require('request-promise-native')
+const rp = require('request-promise-native');
 
-// app.get('/', (req, res) => res.send('Hello World!'))
-
-app.post('/buyGadget', (req, res) => {
-  console.info("buying gadget x!");
-
-  // curl -d 'hello from zsh!' -X POST localhost:8080/events
-
-  var options = {
+const sendEvent = ({event_name, productId, prospectId, additionalInfo}) => {
+  return {
     method: 'POST',
-    uri: 'http://localhost:8080/events',
+    uri: 'http://localhost:8081/events',
     headers: {
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
-      event_name: "prospect_clicked_buy",
-      productId: 123,
-      prospectId: "abc"
+      event_name,
+      productId,
+      prospectId,
+      additionalInfo,
+      reportedTime: moment().format("YYYY-MM-DDTHH:mm:ss.SSS")
     }),
-    // json: true // Automatically stringifies the body to JSON
   };
+};
+
+app.use(function (req, res, next) {
+  console.log('Time:', Date.now());
+  next()
+});
+
+app.post('/buyGadget', (req, res) => {
+  console.info("buying gadget x!");
+
+  const options = sendEvent({
+    event_name: "prospect_clicked_buy",
+    productId: 123,
+    prospectId: "abc",
+    additionalInfo: {
+      "product_name": "Gadget X"
+    },
+  });
 
   rp(options)
     .then(function (parsedBody) {
-      console.log(parsedBody)
+      console.log(parsedBody);
       res.send("purchased!");
       // POST succeeded...
     })
     .catch(function (err) {
-      console.error(err)
+      console.error(err);
       res.send("failed!");
       // POST failed...
     });
 
-})
+});
 
-app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/', function (req, res, next) {
+  console.log('Time:', Date.now());
+  const options = sendEvent({
+    event_name: "prospect_reached_landing_page",
+    productId: 123,
+    prospectId: "abc",
+    additionalInfo: {
+      "landing_page_host": req.headers.host,
+      "landing_page_url": req.url,
+      "user-agent": req.headers["user-agent"],
+    },
+  });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+  rp(options)
+    .then(function (parsedBody) {
+      console.log(parsedBody);
+      // res.send("purchased!");
+      // POST succeeded...
+    })
+    .catch(function (err) {
+      console.error(err);
+      console.error("failed to send analytics to sellify")
+      // POST failed...
+    });
+
+  return express.static(path.join(__dirname, 'public'))(req, res, next)
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
